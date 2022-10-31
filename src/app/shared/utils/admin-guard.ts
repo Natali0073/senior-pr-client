325cf9906506a538bdd6b904911e1d576544880f
Natalia <nataliamakarchuk17@gmail.com>
2022-10-31 20:13:53 +0200

    refactor admin guard

--- src/app/shared/utils/admin-guard.ts
+++ src/app/shared/utils/admin-guard.ts
@@ -1,8 +1,8 @@
 import { Injectable } from "@angular/core";
 import { CanActivate } from "@angular/router";
 import { Store } from "@ngrx/store";
-import { Observable, of } from "rxjs";
-import { catchError, map, switchMap, take, tap } from "rxjs/operators";
+import { iif, Observable, of } from "rxjs";
+import { catchError, map, mergeMap, switchMap, take } from "rxjs/operators";
 import { HomeService } from "src/app/home/home.service";
 import { AppState } from "src/app/state/app.state";
 import { selectCurrentUser } from "src/app/state/users/users.selectors";
@@ -17,19 +17,26 @@ export class AdminGuard implements CanActivate {
   ) { }
 
   canActivate(): Observable<boolean> | Promise<boolean> | boolean {
-    return this.selectUserStore().pipe(
-      switchMap((result) => of(result)),
+    this.getUserRole();
+    return this.getUserRole().pipe(
+      switchMap((result) => of(!!result)),
       catchError(() => of(false))
     );
   }
 
-  selectUserStore(): Observable<boolean> {
-    // return this.store.select(selectCurrentUser)
-    //   .pipe(
-    //     map(user => user && user.role === 'admin' || false),
-    //     take(1)
-    //   )
+  getUserRole() {
+    // check data from the store
+    // if user is absent, fetch data from server
+    return this.store.select(selectCurrentUser)
+      .pipe(
+        map(userData => userData),
+        mergeMap(userData => iif(() => !userData.role, this.fetchUserData(), of(userData && userData.role === 'admin' || false))),
+        take(1)
+      )
 
+  }
+
+  fetchUserData() {
     return this.chatService.getCurrentUser()
       .pipe(
         map(user => user && user.role === 'admin' || false),
