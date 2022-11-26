aa34837d3626405b8f3709d1200634eadea10781
Natalia <nataliamakarchuk17@gmail.com>
2022-11-26 14:26:25 +0200

    add e2e into ci/cd

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'cypress'
+import { environment } from 'src/environments/environment'
 
 export default defineConfig({
   
   e2e: {
-    'baseUrl': 'http://localhost:4200',
+    baseUrl: environment.clientUrl,
     supportFile: false,
   },
   
